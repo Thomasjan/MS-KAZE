35c7e267961868e3477d1e54ccfacd6f2b145fad
ThomasJanko <thomas78125@gmail.com>
2023-11-21 16:11:20 +0100

    test-unit: health check

--- src/tests/health.tests.ts
+++ src/tests/health.tests.ts
@@ -1,9 +1,22 @@
 // sample.test.ts
 import { expect } from 'chai';
+import app from '../index';
 
 describe('Sample Test', () => {
   it('should add two numbers', () => {
     const result = 1 + 1;
     expect(result).to.equal(2);
+  });
+});
+
+describe('API lauch', () => {
+  it('should return 200', async () => {
+    //start the app
+    await app.listen(5000);
+    //make request
+    const result = await fetch('http://localhost:5000/');
+    //expect result
+    expect(result.status).to.equal(200);
+    ;
   });
 });
