fc9a60674e7ff0122ee2c6f51653898f54c6fb8a
Thomas Jankowski <thomas.jankowski@gestimum.com>
2023-12-04 09:06:17 +0100

    jsonMapper to create JSON job + test-unit

--- src/tests/middlewares.tests.ts
+++ src/tests/middlewares.tests.ts
@@ -4,6 +4,11 @@ import app from '../index';
 import validateData from '../utils/validData';
 import express, {Request, Response} from 'express';
 
+import exempleJson from '../data/exemple.json';
+import jsonMapper from '../utils/jsonMapper';
+import exp from 'constants';
+import { assert } from 'console';
+
 chai.use(chaiHttp);
 const { expect } = chai;
 
@@ -37,3 +42,119 @@ describe('validateData Middleware', () => {
       });
   });
 });
+
+
+//jsonMapper Middleware
+describe('jsonMapper Middleware', () => {
+
+  const fields: Object = {
+      ACT_NUMERO: 'test_ACT_NUMERO',
+      PCF_CODE: 'test_PCF_CODE',
+      CCT_NUMERO: 'test_CCT_NUMERO',
+      ACT_OBJET: 'test_ACT_OBJET',
+      ACT_TYPE: 'test_ACT_TYPE',
+      ACT_DESC: 'test_ACT_DESC',
+      ACT_DATE: 'test_ACT_DATE',
+      ACT_DATFIN: 'test_ACT_DATFIN',
+      ACT_DATECH: 'test_ACT_DATECH',
+      PCF_RS: 'test_PCF_RS',
+      PCF_EMAIL: 'test_PCF_EMAIL',
+      PCF_VILLE: 'test_PCF_VILLE',
+      PCF_CP: 'test_PCF_CP',
+      PCF_RUE: 'test_PCF_RUE',
+      XXX_IDMKAZE: 'test_XXX_IDMKAZE',
+  }
+
+  const json: Object = exempleJson;
+
+  const expectedjson = 
+    {
+      "target_id": "company_id",
+      "workflow":{
+          "type": "workflow",
+          "id": "root",
+          "children": [
+            
+            {
+              "type": "template_job_info",
+              "id": "template_info",
+              "label": "Résumé mission",
+              "access": 133,
+              "generate_documents": [
+        
+              ],
+              "city": "test_PCF_VILLE",
+              "state": "IDF",
+              "country_code": "fr",
+              "zip_code": "test_PCF_CP",
+              "job_title": "test_ACT_OBJET",
+              "job_reference": "test_ACT_NUMERO",
+              "job_address": "test_PCF_RUE",
+              "performer_estimation": 60,
+              "files": [
+        
+              ],
+              "children": [
+                {
+                  "type": "section",
+                  "id": "template_section",
+                  "direction": "col",
+                  "children": [
+                    {
+                      "type": "widget_text",
+                      "id": "template_type",
+                      "label": "Code Type d'intervention",
+                      "access": 133,
+                      "data_type": "string",
+                      "data": "test_ACT_TYPE"
+                    },
+                    {
+                      "type": "widget_client",
+                      "id": "template_client",
+                      "label": "Client",
+                      "access": 133,
+                      "city": "test_PCF_VILLE",
+                      "country_code": "fr",
+                      "zip_code": 0,
+                      "client_type": "regular",
+                      "name": "test_PCF_RS",
+                      "email": "test_PCF_EMAIL",
+                      "address_title": "test_PCF_RUE",
+                      "address": "test_PCF_RUE",
+                  },
+                  {
+                    "type": "widget_text",
+                    "id": "template_contact_num",
+                    "label": "Numéro de téléphone",
+                    "access": 133,
+                    "data_type": "phone",
+                  },
+                  {
+                    "type": "widget_text",
+                    "id": "template_description",
+                    "label": "Descriptif",
+                    "access": 133,
+                    "data_type": "string",
+                    "data": "test_ACT_DESC"
+                  }
+                  ]
+                }
+              ]
+            }
+          ]
+      }
+    }
+
+  it('should return the expected JSON', (done) => {
+  const newjson = jsonMapper(json, fields);
+
+  expect(newjson).to.deep.equal(expectedjson);
+  // expected 1+1 = 2
+  // expect(1+1).to.equal(2);
+
+  done();
+  });
+
+});
+        
+
