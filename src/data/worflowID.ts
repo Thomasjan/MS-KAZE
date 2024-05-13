8951a189165fafb9cf56531959c2762d8274f5dd
Thomasjan <thomas.jankowski@gestimum.com>
2024-05-13 15:50:22 +0200

    workflowId

--- src/data/worflowID.ts
+++ src/data/worflowID.ts
@@ -3,8 +3,8 @@ const workflowID = {
     "workflow_id": "",
     "target_id": "my-company-id",
     "data": {
-        "f8816f82-3393-49f4-af78-fbc5d8616cd4": {
-            "f8816f82-3393-49f4-af78-fbc5d8616cd4": {
+        "resume_mission": {
+            "resume_mission": {
                 "job_title": "ACT_OBJET",
                 "job_reference": "ACT_NUMERO",
                 "job_address": "PCF_RUE",
@@ -32,13 +32,13 @@ const workflowID = {
                 "data": "ACT_TYPE"
             }
         },
-        "9cf4cd30-7eed-4c3d-8269-dc32e0928b20":{
+        "vierge":{
             "job_description": {
                 "data": "ACT_DESC"
             }
         },
-        "d25d01b9-dd3b-4030-97ac-bc0f86ac9d7f": {
-            "d25d01b9-dd3b-4030-97ac-bc0f86ac9d7f": {
+        "navigation": {
+            "navigation": {
                 "city": "PCF_VILLE",
                 "country_code": "fr",
                 "zip_code": "PCF_CP",
